52674b7e24565084eae068e1e47afec1e3aaa78d
KojoMensah <boit7845@gmail.com>
2023-08-01 15:49:03 +0000

    reset password page created

---
+++ lib/features/auth/presentation/views/request.password.reset.view.dart
@@ -0,0 +1,136 @@
+import 'package:drivn_app/features/auth/presentation/views/register_screen.dart';
+import 'package:drivn_app/features/auth/presentation/widget/elevated.button.dart';
+import 'package:drivn_app/features/auth/presentation/widget/formfield.dart';
+import 'package:drivn_app/utils/contants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:drivn_app/components/user_inputs/username_input_validator.dart';
+
+import 'otp.input.view.dart';
+
+class RequestPinView extends StatefulWidget {
+  RequestPinView({Key? key}) : super(key: key);
+
+  @override
+  _RequestPinViewState createState() => _RequestPinViewState();
+}
+
+class _RequestPinViewState extends State<RequestPinView> {
+  final FocusNode _focusNodePassword = FocusNode();
+  final TextEditingController _controllerUsername = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+
+  bool _obscurePassword = true;
+
+  final _formKey = GlobalKey<FormState>();
+
+  void signUserIn() {
+    if (_formKey.currentState?.validate() ?? false) {
+      // Form is valid, proceed with sign-in logic
+      // You can access the entered values using _controllerUsername.text and _controllerPassword.text
+      print('Signing in...');
+    }
+  }
+
+  void togglePasswordVisibility() {
+    setState(() {
+      _obscurePassword = !_obscurePassword;
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNodePassword.dispose();
+    _controllerUsername.dispose();
+    _controllerPassword.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blue,
+      appBar: AppBar(
+        title: Text('Forgot Your Password?',
+            style: TextStyle(fontSize: 20, color: Colors.white)),
+        backgroundColor: blue,
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: white,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 50),
+        child: ListView(
+          physics: NeverScrollableScrollPhysics(),
+          children: [
+            const SizedBox(height: 25),
+            Text(
+              "Don't worry! It occurs. Please enter the email address linked with your account.",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 25),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomFormField(
+                    controller: _controllerUsername,
+                    labelText: 'Enter Address',
+                    validator: UsernameInputValidator.validate,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.sizeOf(context).height / 2.3,
+                  ),
+                  CustomElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const OTPInputView(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "Send Code",
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                RegisterView(key: widget.key)),
+                        (route) => false),
+                    child: RichText(
+                      text: TextSpan(
+                        text: "Don't have an account?  ",
+                        style: TextStyle(
+                          color: black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'Register.',
+                            style: TextStyle(
+                              color: yellow,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/presentation/views/reset.password.dart
@@ -0,0 +1,117 @@
+import 'package:drivn_app/features/auth/presentation/views/login_screen.dart';
+import 'package:drivn_app/features/auth/presentation/views/register_screen.dart';
+import 'package:drivn_app/features/auth/presentation/widget/elevated.button.dart';
+import 'package:drivn_app/features/auth/presentation/widget/formfield.dart';
+import 'package:drivn_app/utils/contants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:drivn_app/components/user_inputs/username_input_validator.dart';
+
+import 'otp.input.view.dart';
+
+class ResetPasswordView extends StatefulWidget {
+  ResetPasswordView({Key? key}) : super(key: key);
+
+  @override
+  _ResetPasswordViewState createState() => _ResetPasswordViewState();
+}
+
+class _ResetPasswordViewState extends State<ResetPasswordView> {
+  final FocusNode _focusNodePassword = FocusNode();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _repeatPasswordController =
+      TextEditingController();
+
+  bool _obscurePassword = true;
+  final _formKey = GlobalKey<FormState>();
+
+  void signUserIn() {
+    if (_formKey.currentState?.validate() ?? false) {
+      // Form is valid, proceed with sign-in logic
+      // You can access the entered values using _controllerUsername.text and _controllerPassword.text
+      print('Signing in...');
+    }
+  }
+
+  void togglePasswordVisibility() {
+    setState(() {
+      _obscurePassword = !_obscurePassword;
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNodePassword.dispose();
+    _passwordController.dispose();
+    _repeatPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blue,
+      appBar: AppBar(
+        title: Text('Change Password',
+            style: TextStyle(fontSize: 20, color: Colors.white)),
+        backgroundColor: blue,
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: white,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 50),
+        child: ListView(
+          physics: NeverScrollableScrollPhysics(),
+          children: [
+            const SizedBox(height: 25),
+            Text(
+              "Enter a password not easy to guess. Must not be less than 8 characters.",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 25),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomFormField(
+                    controller: _passwordController,
+                    labelText: 'Enter Password',
+                    validator: UsernameInputValidator.validate,
+                  ),
+                  CustomFormField(
+                    controller: _passwordController,
+                    labelText: 'Repeat Password',
+                    validator: UsernameInputValidator.validate,
+                  ),
+                  CustomElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Password changed successful'),
+                        ),
+                      );
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginView(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "Change Password",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
