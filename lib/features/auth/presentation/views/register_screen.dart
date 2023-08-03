9342abcd0b95db9c861ae18f2f56ee47f785118f
KojoMensah <boit7845@gmail.com>
2023-08-03 22:28:35 +0000

    form field icon colors changed

--- lib/features/auth/presentation/views/register_screen.dart
+++ lib/features/auth/presentation/views/register_screen.dart
@@ -55,129 +55,108 @@ class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF554AF0),
+      backgroundColor: blue,
       body: Padding(
-        padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 20),
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: Image.asset(
-                "assets/logo.png",
-                fit: BoxFit.cover,
-              ),
-            ),
-            Center(
-              child: Text(
-                'Create account to get started!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
+        padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 10),
+        child: SafeArea(
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Image.asset(
+                  "assets/logo.png",
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-            const SizedBox(height: 32),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  CustomFormField(
-                    controller: _nameController,
-                    labelText: 'First Name',
-                    prefixIcon: Icon(Icons.person_2_outlined),
+              Center(
+                child: Text(
+                  'Create account to get started!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
                   ),
-                  CustomFormField(
-                    controller: _nameController,
-                    labelText: 'Last Name',
-                    prefixIcon: Icon(Icons.person_2_outlined),
-                  ),
-                  CodePickerField(
-                    controller: _emailController,
-                  ),
-                  CustomFormField(
-                    controller: _passwordController,
-                    labelText: 'Password',
-                    prefixIcon: Icon(Icons.password_outlined),
-                    suffixIcon: Icons.visibility,
-                  ),
-                  CustomFormField(
-                    controller: _repeatPasswordController,
-                    labelText: 'Repeat password',
-                    prefixIcon: Icon(Icons.password_outlined),
-                    suffixIcon: Icons.visibility,
-                  ),
-                  // const SizedBox(height: 10),
-                  CustomElevatedButton(
-                    onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const OTPInputView(),
-                      ),
-                    ),
-                    child: Text('Register'),
-                  ).loading(false),
-                  const SizedBox(height: 24),
-                  GestureDetector(
-                    onTap: () => Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(
-                          builder: (context) => LoginView(key: widget.key)),
-                      (route) => false,
-                    ),
-                    child: RichText(
-                      text: TextSpan(
-                        text: 'Already have an account? ',
-                        style: TextStyle(
-                          color: black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                        children: [
-                          TextSpan(
-                            text: 'Login',
-                            style: TextStyle(
-                              color: yellow,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  GoogleButton(
-                    onTap: () {},
-                    title: 'Sign up',
-                  )
-                ],
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 32),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    CustomFormField(
+                      controller: _nameController,
+                      labelText: 'First Name',
+                      prefixIcon: Icon(Icons.person_2_outlined),
+                    ),
+                    CustomFormField(
+                      controller: _nameController,
+                      labelText: 'Last Name',
+                      prefixIcon: Icon(Icons.person_2_outlined),
+                    ),
+                    CodePickerField(
+                      controller: _emailController,
+                    ),
+                    CustomFormField(
+                      controller: _passwordController,
+                      labelText: 'Password',
+                      prefixIcon: Icon(Icons.password_outlined),
+                      suffixIcon: Icons.visibility,
+                    ),
+                    CustomFormField(
+                      controller: _repeatPasswordController,
+                      labelText: 'Repeat password',
+                      prefixIcon: Icon(Icons.password_outlined),
+                      suffixIcon: Icons.visibility,
+                    ),
+                    // const SizedBox(height: 10),
+                    CustomElevatedButton(
+                      onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const OTPInputView(),
+                        ),
+                      ),
+                      child: Text('Register'),
+                    ).loading(false),
+                    const SizedBox(height: 24),
+                    GestureDetector(
+                      onTap: () => Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(
+                            builder: (context) => LoginView(key: widget.key)),
+                        (route) => false,
+                      ),
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Already have an account? ',
+                          style: TextStyle(
+                            color: black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: 'Login',
+                              style: TextStyle(
+                                color: yellow,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    GoogleButton(
+                      onTap: () {},
+                      title: 'Sign up',
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  }
-
-//this widget is responsible for picking country codes
-  Widget countryCodePicker(BuildContext context) {
-    return GestureDetector(
-      onTap: () async {
-        // Show the country code picker when tapped.
-        final cCode = await countryPicker.showPicker(context: context);
-        // Null check
-        if (cCode != null) {
-          code = cCode;
-          print(code!.code);
-        }
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 6.0,
-        ),
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: const BoxDecoration(
-            color: yellow,
-            borderRadius: BorderRadius.all(Radius.circular(5.0))),
-        child:
-            Text('country code', style: const TextStyle(color: Colors.white)),
       ),
     );
   }
