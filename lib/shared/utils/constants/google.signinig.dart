1b0aeb02f2894dac489fb6553000004cb4a10574
KojoMensah <boit7845@gmail.com>
2023-10-05 15:20:24 +0000

    working with google signing

---
+++ lib/shared/utils/constants/google.signinig.dart
@@ -0,0 +1,27 @@
+// import 'dart:io';
+
+// const REFRESH_TOKEN_KEY = 'refresh_token';
+// const BACKEND_TOKEN_KEY = 'backend_token';
+// const GOOGLE_ISSUER = 'https://accounts.google.com';
+// const GOOGLE_CLIENT_ID_IOS = '707888038435-h1jtdkqat3punrqqpcv39vbltr9ai2rj.apps.googleusercontent.com';
+// const GOOGLE_REDIRECT_URI_IOS = 'com.googleusercontent.apps.<IOS-CLIENT-ID>:/oauthredirect';
+// const GOOGLE_CLIENT_ID_ANDROID = '707888038435-9vuvrq33uammvdvjq62fa8jk7bjc5bh5.apps.googleusercontent.com';
+// const GOOGLE_REDIRECT_URI_ANDROID = 'com.googleusercontent.apps.<ANDROID-CLIENT-ID>:/oauthredirect';
+
+// String clientID() {
+//   if(Platform.isAndroid) {
+//     return GOOGLE_CLIENT_ID_ANDROID;
+//   } else if (Platform.isIOS) {
+//     return GOOGLE_CLIENT_ID_IOS;
+//   }
+//   return '';
+// }
+
+// String redirectUrl() {
+//   if(Platform.isAndroid) {
+//     return GOOGLE_REDIRECT_URI_ANDROID;
+//   } else if (Platform.isIOS) {
+//     return GOOGLE_REDIRECT_URI_IOS;
+//   }
+//   return '';
+// }
